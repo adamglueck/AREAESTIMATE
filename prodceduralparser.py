b739f0b38cd0b0041096c8c3c71caead7c8f2bd4
adamglueck <adam.glueck1@gmail.com>
2016-05-27 07:27:18 -0400

    Updated from Brython Server: May 27, 2016 7:27:21 AM EDT

--- prodceduralparser.py
+++ prodceduralparser.py
@@ -3,7 +3,12 @@
 from math import sin, cos, log, pi, e
 function=list(str(input("function in terms of x "))
 equivs=[('ln','log'),('^','**'),('1x','1*x'),('2x','2*x'),('3x','3*x'),('4x','4*x'),('5x','5*x'),('6x','6*x'),('7x','7*x'),('8x','8*x'),('9x','9*x'),('0x','0*x'),('sec','1/cos'),('csc','1/sin'),('cot','1/tan')]
-print equivs
+print (equivs)
+for i in range (0,len(equivs)):
+    searcher=list(equivs[i][0])
+    replacer=list(equivs[i][0]
+    print(searcher)
+    print(replacer)
 """
 for i in range (0,len(f)-1):
     if f[i]=='l' and f[i+1]=='n':
