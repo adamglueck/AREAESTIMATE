2d8ccd22dd1c6a8b30984a6873da1c4f0c5ff52d
adamglueck <adam.glueck1@gmail.com>
2016-05-27 07:23:07 -0400

    Updated from Brython Server: May 27, 2016 7:23:11 AM EDT

--- prodceduralparser.py
+++ prodceduralparser.py
@@ -1,9 +1,10 @@
 
 
 from math import sin, cos, log, pi, e
-function=""
-f=str(input("function in terms of x "))
-f=list(f)
+function=list(str(input("function in terms of x "))
+equivs=[('ln','log'),('^','**'),('1x','1*x'),('2x','2*x'),('3x','3*x'),('4x','4*x'),('5x','5*x'),('6x','6*x'),('7x','7*x'),('8x','8*x'),('9x','9*x'),('0x','0*x'),('sec','1/cos'),('csc','1/sin'),('cot','1/tan')]
+print equivs
+"""
 for i in range (0,len(f)-1):
     if f[i]=='l' and f[i+1]=='n':
         f[i+1]='g'
@@ -49,3 +50,4 @@ d=str(d)
 print("MRAM is "+d)
 e=str(2*(float(d)+float(c))/3)
 print("SIMPSON'S is "+e)
+"""
