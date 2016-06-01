f8e48dfbb87be931597c1c83b6e27153685c3d55
adamglueck <adam.glueck1@gmail.com>
2016-06-01 13:35:45 -0400

    Updated from Brython Server: June 1, 2016 1:35:45 PM EDT

--- euler.py
+++ euler.py
@@ -1,10 +1,12 @@
 startingvaluex=float(input("starting value x? "))
+endingvaluex=float(input("for what x value shall I estimate y? "))
 startingvaluey=float(input("starting value y? "))
 step=float(input("step? "))
 deriv=str(input("differential equation? "))
 x=startingvaluex
 y=startingvaluey
-while x>startingvaluex:
+
+while endingvaluex>y:
   y=y+step*(eval(deriv))
   x=x+step
   print(x, y)
