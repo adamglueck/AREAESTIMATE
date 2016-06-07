5a8c1382c6bd6baf0a9f48c991082ebf4f718478
adamglueck <adam.glueck1@gmail.com>
2016-06-07 07:50:24 -0400

    Update com.py

--- com.py
+++ com.py
@@ -41,38 +41,41 @@ for i in range (0,num):
         totale=e+totale
     ymin=float(input("y minimum of piecewise component"))
     ymax=float(input("y maximum of piecewise component"))
-    f2=list(input("piecewise function component in terms of y "))
-    for i in range (0,len(f2)-1):
-        if f2[i]=='l' and f2[i+1]=='n':
-            f2[i+1]='g'
-            f2.insert(i+1,'o')
+    f=list(str(input("piecewise function component in terms of y ")))
     for i in range (0,len(f)):
-        if f2[i]=='^':
-            f2[i]='*'
-            f2.insert(i+1,'*')
-    for i in range (0,len(f2)-1):
-        if f2[i]=='1'and f2[i+1]=='y' or f2[i]=='2'and f2[i+1]=='y' or f2[i]=='3'and f2[i+1]=='y' f2[i]=='4'and f2[i+1]=='y' or f2[i]=='5'and f2[i+1]=='y' or f2[i]==6and f2[i+1]=='y' or f2[i]=='7'and f2[i+1]=='y' or f2[i]=='8'and f2[i+1]=='y' or f2[i]=='9'and f2[i+1]=='y' or f2[i]=='0' and f2[i+1]=='y':
-            f2.insert(i+1,'*')
-    for i in range (0,len(f2)-2):
-        if f2[i]=='1' and f2[i+1]=='y' or f2[i]=='2' and f2[i+1]=='y' or f2[i]=='3' and f2[i+1]=='y' f2[i]=='4' and f2[i+1]=='y'  or f2[i]=='5' and f2[i+1]=='y' or f2[i]==6 and f2[i+1]=='y'or f2[i]=='7' and f2[i+1]=='y' or f2[i]=='8' and f2[i+1]=='y' or f2[i]=='9' and f2[i+1]=='y' or f2[i]=='0' and f2[i+1]=='l' and f2[i+2]=='n':
-            f2.insert(i+1,'*')
-    equation3=""
-    for i in range (0,len(f2)):
-        equation=equation+f2[i]
-    equation4="y*("+equation+")"
+        if f[i]=='y':
+            f[i]='x'
+    for i in range (0,len(f)-1):
+        if f[i]=='l' and f[i+1]=='n':
+            f[i+1]='g'
+            f.insert(i+1,'o')
+    for i in range (0,len(f)):
+        if f[i]=='^':
+            f[i]='*'
+            f.insert(i+1,'*')
+    for i in range (0,len(f)-1):
+        if f[i]=='1'and f[i+1]=='x' or f[i]=='2'and f[i+1]=='x' or f[i]=='3'and f[i+1]=='x' f[i]=='4'and f[i+1]=='x' or f[i]=='5'and f[i+1]=='x' or f[i]==6and f[i+1]=='x' or f[i]=='7'and f[i+1]=='x' or f[i]=='8'and f[i+1]=='x' or f[i]=='9'and f[i+1]=='x' or f[i]=='0' and f[i+1]=='x':
+            f.insert(i+1,'*')
+    for i in range (0,len(f)-2):
+        if f[i]=='1' and f[i+1]=='x' or f[i]=='2' and f[i+1]=='x' or f[i]=='3' and f[i+1]=='x' f[i]=='4' and f[i+1]=='x'  or f[i]=='5' and f[i+1]=='x' or f[i]==6 and f[i+1]=='x'or f[i]=='7' and f[i+1]=='x' or f[i]=='8' and f[i+1]=='x' or f[i]=='9' and f[i+1]=='x' or f[i]=='0' and f[i+1]=='l' and f[i+2]=='n':
+            f.insert(i+1,'*')
+    equation=""
+    for i in range (0,len(f)):
+        equation=equation+f[i]
+    equation2="x*("+equation+")"
     f=0
-    step=(ymax-ymin)/500
+    step=(xmax-xmin)/500
     for i in range (0,500):
-        i=(ymin+step/2)+step*i
-        y=i
+        i=(xmin+step/2)+step*i
+        x=i
         f=f+(eval(equation2))*step
     g=0
-    for j in range (0,500):
-        j=(ymin+step/2)+step*j
-        y=j
+    for i in range (0,500):
+        i=(xmin+step/2)+step*i
+        x=i
         g=g+(eval(equation))*step
-        totalg=g*(ymax-ymin)/2+totalg
-        totalf=f+totalf
+        totalf=f*(xmax-xmin)/f+totald
+        totalg=g+totalg
 if num==1:
     print(d/e)
     print(f/g)
